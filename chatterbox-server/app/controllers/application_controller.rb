a0322ae4f2415abccba271a69dcb29f62d6d7ea5
Ambar Gonzalez <49761975+Ambarpg80@users.noreply.github.com>
2023-04-21 11:39:00 -0400

    commit tree adb1544ab3f4573ece539a4c4bd87635d0cf362b

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,35 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
-  
   # Add routes
+
+  #GET (Read)
+  get "/messages" do 
+    message = Message.order("created_at ASC")
+    message.to_json
+  end
+
+  #POST (Create)
+  post "/messages" do 
+    message = Message.create(
+      username: params[:username],
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  #PATCH (Update)
+  patch "/messages/:id" do 
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  #DELETE (Destroy)
+  delete "/messages/:id" do
+    message = Message.find(params[:id])
+    message.destroy 
+  end
   
 end
